35b8bba87ea09650077a77f5d556bb5638c84ed9
Deep Sorathiya <deep.professional803@gmail.com>
2025-05-31 07:24:52 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,17 @@
+public class Basic 
+{
+    public static void main(String args[])
+    {
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(~5);
+        System.out.println(5 ^ 6);
+        System.out.println(5<<4);
+        System.out.println(5<<2);
+        System.out.println(5>>2);
+        System.out.println(100>>2);
+        System.out.println(6>>2);
+
+        System.out.println((~0)+ " " +(~1)+ " " +(~5)+ " " +(~6));
+    }
+}
